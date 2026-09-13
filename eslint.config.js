b0602c2e5ddb77a4e04ff6f219499bcfb5137904
Oliver Wycisk <development@o-wycisk.de>
2026-09-13 13:06:05 +0200

    feat: vite 8

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['src/**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                setTimeout: 'readonly',
+                URL: 'readonly',
+            },
+        },
+    },
+]
